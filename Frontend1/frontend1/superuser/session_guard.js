6a63419bd6f1c742fd68cb1303d91662f22f19ee
samridhi chourasia <samridhichourasia338@gmail.com>
2026-05-03 15:16:22 +0530

    Delete Frontend1 directory

--- Frontend1/frontend1/superuser/session_guard.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var user = null;
-  try { user = JSON.parse(sessionStorage.getItem('loggedInUser') || 'null'); } catch(e){}
-  if (!user) { window.location.href = '../applicant_connected/connected/login/index.html'; return; }
-  if (user.role && user.role.toLowerCase() !== 'superuser') {
-    window.location.href = '../applicant_connected/connected/login/index.html';
-  }
-})();
